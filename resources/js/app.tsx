db5a4b95ff701cce16441c1f529bbbbe59488e3c
Rustian Afencius Marbun <afenmarbun@gmail.com>
2025-08-17 09:12:46 +0700

    fix: register pusher client for echo

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,19 +1,23 @@
 import '../css/app.css';
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';import 'leaflet/dist/leaflet.css';
 import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { registerSW } from 'virtual:pwa-register';
 import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
 registerSW();
 
 declare global {
     interface Window {
         Echo: Echo;
+        Pusher: typeof Pusher;
     }
 }
 
+window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
